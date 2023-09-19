bd7c63e42b413c6595be20c9907284fe736282b8
Pounct <53088375+pounct@users.noreply.github.com>
2023-09-19 11:28:52 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+package ejercici;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner sc = new Scanner(System.in);
+	static Integer[] meses30 = { 1, 3, 5, 7, 8, 10, 12 };
+	static ArrayList<Integer> listm30 = new ArrayList<Integer>(Arrays.asList(meses30));
+	static Integer[] meses31 = { 4, 6, 9, 11 };
+	static ArrayList<Integer> listm31 = new ArrayList<Integer>(Arrays.asList(meses31));
+	static int[] mes02 = { 28, 29 };
+
+	public static void main(String[] args) {
+		System.out.println("Entrar una fecha xx/xx/xxxx");
+		String entrada = sc.nextLine();
+		String[] los3 = entrada.split("/");
+		int dia = Integer.parseInt(los3[0]);
+		int mes = Integer.parseInt(los3[1]);
+		int ani = Integer.parseInt(los3[2]);
+
+		if (mes != 2) {
+			if (listm30.contains(mes)) {
+				if (dia <= 30 && dia >= 1) {
+					System.out.println("correcta");
+				} else {
+					System.out.println("error");
+				}
+
+			} else if (listm31.contains(mes)) {
+				if (dia <= 31 && dia >= 1) {
+					System.out.println("correcta");
+				} else {
+					System.out.println("error");
+				}
+
+			}
+
+		} else {
+			if (((ani % 4 == 0) && (ani % 100 != 0)) || (ani % 400 == 0)) {
+				if (dia <= 28 && dia >= 1) {
+					System.out.println("correcta");
+				} else {
+					System.out.println("error");
+				}
+			}
+		}
+
+	}
+
+}
